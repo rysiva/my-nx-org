a12b37eb84cb4d2a38903dd0d113af33a9c038e5
rysiva <race.lifes.a.race@gmail.com>
2022-09-11 19:44:15 +0900

    Initial commit

---
+++ apps/todos/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-nx-org-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'todos';
+}
