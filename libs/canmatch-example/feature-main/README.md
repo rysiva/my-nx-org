e0cb99639142199f3b8a29d2f1b6fa8357e5b031
rysiva <50093319+rysiva@users.noreply.github.com>
2022-12-05 19:03:20 +0900

    feat(canmatch-example): generate library

---
+++ libs/canmatch-example/feature-main/README.md
@@ -0,0 +1,7 @@
+# canmatch-example-feature-main
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test canmatch-example-feature-main` to execute the unit tests.
---
+++ libs/canmatch-example/feature-main/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { FeatureMainComponent } from './canmatch-example-feature-main/canmatch-example-feature-main.component';
+
+export const canmatchExampleFeatureMainRoutes: Route[] = [
+  { path: '', component: FeatureMainComponent },
+];
